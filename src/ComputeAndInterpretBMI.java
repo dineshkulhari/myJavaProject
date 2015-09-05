e55f1478a6051efe447703da7ac6621f03dbc9cd
Dinesh Kulhari <dinesh.kulhari@gmail.com>
2015-09-05 22:05:44 +0400

    Testing files

---
+++ src/ComputeAndInterpretBMI.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Created by Dinesh on 9/5/2015.
+ */
+public class ComputeAndInterpretBMI {
+    public static void main(String[] args) {
+
+        // Constant
+        final double KILOGRAM_PER_POUND = 0.453593 ;
+        final double METERS_PER_INCH = 0.0254 ;
+
+        // Creating a Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Enter the weight in Pound
+        System.out.println("Enter the weight Pound");
+        double weight = input.nextDouble();
+
+        // Enter the height
+        System.out.println("Enter the hight in Meter");
+        double height = input.nextDouble();
+
+
+
+
+
+    }
+}
